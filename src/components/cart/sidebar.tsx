10895776f334a03cb2a8818a3eb41d52ec0dfa08
Hugo Souza <hugo.html.com@gmail.com>
2024-06-13 22:05:38 -0300

    feat: Adicionar componente de quantidade

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,16 +1,29 @@
-
+"use client"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { CartItem } from "./item";
+
 
 export const CartSidebar = () =>{
+    //Puxando carrinho do zustant
+    const { cart } = useCartStore(state => state);
+
+    let subTotal = 0;
+    for(let item of cart) {
+        subTotal+=item.quantity * item.product.price;
+    }
     return(
         <Sheet>
             <SheetTrigger asChild>
-                <Button>
+                <Button className="relative">
                     <RocketIcon className="mr-2"/>
                     <p>Carrinho</p>
+                    {cart.length > 0 &&
+                        <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1"></div>
+                    }
                 </Button>
             </SheetTrigger>
             <SheetContent>
@@ -18,16 +31,18 @@ export const CartSidebar = () =>{
                     <SheetTitle>Carrinho</SheetTitle>
                 </SheetHeader>
                 <div className="flex flex-col gap-5 my-3">
-                    Produtos
+                    {cart.map(item=>(
+                        <CartItem key={item.product.id} item={item}/>
+                    ))}
                 </div>
                 <Separator className="my-4"/>
                 <div className="flex justify-between items-center text-xs">
-                    <div>Subuotal:</div>
-                    <div>...</div>
+                    <div>Subtotal:</div>
+                    <div>R$: {subTotal.toFixed(2)}</div>
                 </div>
                 <Separator className="my-4"/>
                 <div className="text-center">
-                    <Button>Finalizar Comprar</Button>
+                    <Button disabled={cart.length === 0}>Finalizar Comprar</Button>
                 </div>
             </SheetContent>
         </Sheet>
